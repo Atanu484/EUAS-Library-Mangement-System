50a44eec3f03f18757e67f3fd11f71f92f72338c
Atanu Biswas <82307753+Atanu484@users.noreply.github.com>
2022-11-15 16:26:41 +0200

    Update wsgi.py

--- euas_library/wsgi.py
+++ euas_library/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'diulibrary.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'euaslibrary.settings')
 
 application = get_wsgi_application()
